ea9e0a6b3fd45111c7dc1176ce05895bbb5c2a46
Harvey Sanders <sanders.harvey@gmail.com>
2023-03-09 10:18:37 -0500

    Discussion notes

--- 0-smoke-test/main_test.go
+++ 0-smoke-test/main_test.go
@@ -14,6 +14,10 @@ func TestSmokeTest(t *testing.T) {
 	// TODO: Fix Test
 	// Server code passes the protohacker tests, but this test times out because the client.Read() below blocks and never completes.
 	// Need to investigate why.
+	//  https://go.dev/play/p/hoRi3-gkei4
+	// Discussion https://gophers.slack.com/archives/C02A8LZKT/p1678373886731539
+	// https://go.dev/play/p/P68iiBHEC0Y
+	// https://go.dev/play/p/pwg0LmkQoY_K
 	t.Run("echos data sent from client", func(t *testing.T) {
 		// Create a pipe to simulate a TCP network connection without starting a server.
 		// https://stackoverflow.com/a/41668611/5275148
@@ -24,18 +28,23 @@ func TestSmokeTest(t *testing.T) {
 		client.SetReadDeadline(time.Now().Add(time.Second / 2))
 
 		// Run the server logic in another goroutine
+		done := make(chan struct{})
 		go func() {
+			defer close(done)
 			handleConnection(server, 0)
 		}()
 
 		want := []byte("helloooo")
-		got := make([]byte, len(want))
+		wantLen := int64(len(want))
+		got := make([]byte, wantLen)
 
 		// Write message to client (send to server)
 		// LimitReader used to simulate io.EOF after writing the message.
-		r := io.LimitReader(bytes.NewReader(want), int64(len(want)))
-		_, err := io.Copy(client, r)
+		r := io.LimitReader(bytes.NewReader(want), wantLen)
+		nbw, err := io.Copy(client, r)
 		require.NoError(t, err)
+
+		require.Equal(t, nbw, wantLen, "should write bytes to client")
 
 		// Read response from server
 		_, err = client.Read(got)
@@ -44,5 +53,6 @@ func TestSmokeTest(t *testing.T) {
 		require.Equal(t, want, got)
 
 		client.Close()
+		<-done
 	})
 }
