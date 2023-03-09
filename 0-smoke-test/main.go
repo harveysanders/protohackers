c2a021e46aeb03dc27e4d339c512b469de505c74
Harvey Sanders <sanders.harvey@gmail.com>
2023-03-09 04:01:34 -0500

    Echo bytes back on connection

--- 0-smoke-test/main.go
+++ 0-smoke-test/main.go
@@ -44,14 +44,23 @@ func handleConnection(conn net.Conn, clientID int) {
 	var buf bytes.Buffer
 	bytesRead, err := io.Copy(&buf, conn)
 	if err != nil {
-		log.Printf("id [%d]: copy: %s", clientID, err.Error())
+		log.Printf("[%d]: copy: %s", clientID, err.Error())
 		return
 	}
 
-	log.Printf("id [%d]: read in %d bytes", clientID, bytesRead)
+	log.Printf("[%d]: read in %d bytes", clientID, bytesRead)
+
+	bytesWritten, err := conn.Write(buf.Bytes())
+	if err != nil {
+		log.Printf("[%d]: write: %s", clientID, err.Error())
+		return
+	}
+
+	log.Printf("[%d]: wrote out %d bytes", clientID, bytesWritten)
+
 	err = conn.Close()
 	if err != nil {
-		log.Printf("id [%d]: close: %s", clientID, err.Error())
+		log.Printf("[%d]: close: %s", clientID, err.Error())
 		return
 	}
 }
