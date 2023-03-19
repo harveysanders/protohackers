f7b53069488c35022161ded8205fb88edc459135
Harvey Sanders <sanders.harvey@gmail.com>
2023-03-19 16:08:08 -0400

    Initial DB attempt

--- 4-unusual-database-program/udb.go
+++ 4-unusual-database-program/udb.go
@@ -1,10 +1,12 @@
 package udb
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"log"
 	"net"
+	"sync"
 	"time"
 )
 
@@ -13,6 +15,11 @@ type (
 		readTimeout   time.Duration
 		writeTimeout  time.Duration
 		maxBufferSize int
+		store         *store
+	}
+
+	store struct {
+		store sync.Map
 	}
 )
 
@@ -21,6 +28,7 @@ func NewServer() *server {
 		readTimeout:   time.Second * 10,
 		writeTimeout:  time.Second * 10,
 		maxBufferSize: 1024,
+		store:         newStore(),
 	}
 }
 
@@ -32,17 +40,28 @@ func (s *server) ServeUDP(ctx context.Context, address string) error {
 	defer pConn.Close()
 
 	done := make(chan error, 1)
-	incoming := make([]byte, s.maxBufferSize)
+	buffer := make([]byte, s.maxBufferSize)
 
 	go func() {
 		for {
-			n, fromAddr, err := pConn.ReadFrom(incoming)
+			n, fromAddr, err := pConn.ReadFrom(buffer)
 			if err != nil {
 				done <- fmt.Errorf("readFrom: %w", err)
 				return
 			}
 
-			log.Printf("incoming packet from %s..len: %d\ncontents: %s\n", fromAddr.String(), n, incoming)
+			msg := bytes.Trim(buffer[:n], "\n")
+			log.Printf("incoming packet!\nfrom: %s\nlen: %d\ncontents: %s\n\n", fromAddr.String(), n, msg)
+
+			if IsInsert(msg) {
+				if err := s.handleInsert(msg); err != nil {
+					done <- fmt.Errorf("handleInsert: %w", err)
+					return
+				}
+				continue
+			}
+			// Assume it's a retrieve request
+			resp := s.handleRetrieve(msg)
 
 			err = pConn.SetWriteDeadline(time.Now().Add(s.writeTimeout))
 			if err != nil {
@@ -50,13 +69,13 @@ func (s *server) ServeUDP(ctx context.Context, address string) error {
 				return
 			}
 
-			pConn.WriteTo(incoming[:n], fromAddr)
+			pConn.WriteTo(resp, fromAddr)
 			if err != nil {
 				done <- err
 				return
 			}
 
-			log.Printf("wrote echo packet to %s..len: %d\n", fromAddr.String(), n)
+			log.Printf("wrote response packet!\nto: %s\nlen: %d\n\n", fromAddr.String(), n)
 		}
 	}()
 
@@ -68,4 +87,54 @@ func (s *server) ServeUDP(ctx context.Context, address string) error {
 	}
 
 	return nil
+}
+
+func IsInsert(data []byte) bool {
+	return bytes.ContainsRune(data, '=')
+}
+
+func (s *server) handleInsert(q []byte) error {
+	pair := bytes.SplitN(q, []byte("="), 2)
+	if len(pair) != 2 {
+		return fmt.Errorf("could not parse query: %s\n%+v", q, pair)
+	}
+	key := pair[0]
+	value := pair[1]
+	s.store.insert(key, value)
+	return nil
+}
+
+func (s *server) handleRetrieve(key []byte) (resp []byte) {
+	v, ok := s.store.retrieve(key)
+	if !ok {
+		// If a request attempts to retrieve a key for which no value exists, the server can return a response as if the key had the empty value (e.g. "key=")
+		return append(key, '=')
+	}
+	resp = append(resp, key...)
+	resp = append(resp, '=')
+	resp = append(resp, v...)
+	return resp
+}
+
+func newStore() *store {
+	return &store{
+		store: sync.Map{},
+	}
+}
+
+func (s *store) insert(k []byte, v []byte) {
+	s.store.Store(string(k), v)
+}
+
+func (s *store) retrieve(k []byte) (value []byte, ok bool) {
+	v, ok := s.store.Load(string(k))
+	if !ok {
+		return []byte{}, false
+	}
+	bv, ok := v.([]byte)
+	if !ok {
+		log.Printf("could not convert value, %+v, to byte slice", v)
+		return []byte{}, false
+	}
+	return bv, true
 }
--- 4-unusual-database-program/udb_test.go
+++ 4-unusual-database-program/udb_test.go
@@ -1,9 +1,12 @@
 package udb_test
 
 import (
+	"context"
 	"net"
 	"testing"
+	"time"
 
+	"github.com/harveysanders/protohackers/udb"
 	"github.com/stretchr/testify/require"
 )
 
@@ -12,10 +15,50 @@ func TestServer(t *testing.T) {
 	raddr, err := net.ResolveUDPAddr("udp", remoteAddr)
 	require.NoError(t, err)
 
+	srv := udb.NewServer()
+	ctx, cancel := context.WithCancel(context.Background())
+	go srv.ServeUDP(ctx, remoteAddr)
+
 	t.Run("accepts an insert request", func(t *testing.T) {
+		time.Sleep(time.Second / 2)
 		conn, err := net.DialUDP("udp", nil, raddr)
 		require.NoError(t, err)
 		defer conn.Close()
 
+		n, err := conn.Write([]byte("foo=bar"))
+		require.NoError(t, err)
+		require.Greater(t, n, 0)
+
+		n, err = conn.Write([]byte("foo"))
+		require.NoError(t, err)
+		require.Greater(t, n, 0)
+
+		want := []byte("foo=bar")
+		resp := make([]byte, len(want))
+		n, addr, err := conn.ReadFrom(resp)
+		require.NoError(t, err)
+		require.Greater(t, n, 0)
+		require.Equal(t, "127.0.0.1:9002", addr.String())
+		require.Equal(t, want, resp)
+
 	})
+
+	time.Sleep(time.Second)
+	cancel()
+}
+
+func TestIsInsert(t *testing.T) {
+	testCases := []struct {
+		data []byte
+		want bool
+	}{
+		{[]byte("hello=world"), true},
+	}
+
+	for _, tc := range testCases {
+		t.Run("identify Inserts and Retrievals", func(t *testing.T) {
+			got := udb.IsInsert(tc.data)
+			require.Equal(t, tc.want, got)
+		})
+	}
 }
