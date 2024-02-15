ec7cc8da82c5ab08a33d08e5ee282adbbdeb49db
Harvey Sanders <sanders.harvey@gmail.com>
2024-02-15 13:42:05 -0500

    Delete job

--- 9-job-centre/inmem/inmem.go
+++ 9-job-centre/inmem/inmem.go
@@ -15,23 +15,11 @@ var (
 	ErrNoJob = errors.New("no-job")
 )
 
-type jobState int
-
-const (
-	JobStateUnAssigned jobState = iota // Job not yet assigned to a worker. Default status.
-	JobStateAssigned                   // Job assigned to a worker.
-	JobStateDeleted                    // Job deleted. Can not be reassigned or retrieved by clients.
-	JobStateAborted                    // Job aborted. Once a job is aborted, it can be reassigned to any client. Jobs are automatically aborted when that client disconnects.
-)
-
 // job represents a job in a queue.
 type job struct {
 	ID      uint64          // Unique identifier.
 	Pri     int64           // Priority. Higher value has higher priority.
 	Payload json.RawMessage // JSON serialized data associated with job
-
-	state    jobState // Current status of the job.
-	workerID *int     // ID of the worker that has been assigned the job, nil if not assigned.
 }
 
 type Store struct {
@@ -63,7 +51,7 @@ func (q *Store) nextID() uint64 {
 }
 
 // AddJob adds a job to the named queue.
-func (q *Store) AddJob(ctx context.Context, userID uint64, queueName string, pri int64, payload json.RawMessage) error {
+func (q *Store) AddJob(ctx context.Context, clientID uint64, queueName string, pri int64, payload json.RawMessage) (job, error) {
 	id := q.nextID()
 	q.qMu.Lock()
 	defer q.qMu.Unlock()
@@ -72,13 +60,12 @@ func (q *Store) AddJob(ctx context.Context, userID uint64, queueName string, pri
 		ID:      id,
 		Pri:     pri,
 		Payload: payload,
-		state:   JobStateUnAssigned,
 	}
 
 	curQ, ok := q.queues[queueName]
 	if !ok {
 		q.queues[queueName] = []job{newJob}
-		return nil
+		return newJob, nil
 	}
 
 	index := slices.IndexFunc(curQ, func(j job) bool {
@@ -93,18 +80,22 @@ func (q *Store) AddJob(ctx context.Context, userID uint64, queueName string, pri
 	}
 
 	q.queues[queueName] = curQ
-	return nil
+	return newJob, nil
 }
 
 // NextJob retrieves the highest priority job of all the named queues.
-func (s *Store) NextJob(ctx context.Context, userID uint64, queueNames []string) (job, string, error) {
+func (s *Store) NextJob(ctx context.Context, clientID uint64, queueNames []string) (job, string, error) {
 	highestPriJob := job{Pri: math.MinInt64}
 	queueName := ""
 	for _, name := range queueNames {
 		j, err := s.peek(ctx, name)
 		if err != nil {
+			if err == ErrNoJob {
+				continue
+			}
 			return job{}, "", err
 		}
+
 		if j.Pri > highestPriJob.Pri {
 			highestPriJob = j
 			queueName = name
@@ -115,7 +106,7 @@ func (s *Store) NextJob(ctx context.Context, userID uint64, queueNames []string)
 		return job{}, "", ErrNoJob
 	}
 
-	_, err := s.dequeue(ctx, userID, queueName)
+	_, err := s.dequeue(ctx, clientID, queueName)
 	if err != nil {
 		return job{}, "", fmt.Errorf("s.dequeue: %w", err)
 	}
@@ -137,7 +128,7 @@ func (s *Store) peek(ctx context.Context, queueName string) (job, error) {
 }
 
 // dequeue remove the job from the queue and adds it to the store's assigned map.
-func (s *Store) dequeue(ctx context.Context, userID uint64, queueName string) (job, error) {
+func (s *Store) dequeue(ctx context.Context, clientID uint64, queueName string) (job, error) {
 	s.qMu.Lock()
 	defer s.qMu.Unlock()
 	q, ok := s.queues[queueName]
@@ -157,4 +148,46 @@ func (s *Store) dequeue(ctx context.Context, userID uint64, queueName string) (j
 	q = slices.Delete(q, 0, 1)
 	s.queues[queueName] = q
 	return job, nil
+}
+
+// DeleteJob deletes a job from the store. Any client can delete a job,
+// even if it was worked on by another client.
+func (s *Store) DeleteJob(ctx context.Context, clientID uint64, jobID uint64) error {
+	_, _, err := s.deleteJobByID(ctx, jobID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Store) deleteJobByID(ctx context.Context, jobID uint64) (job, string,
+	error) {
+	s.qMu.Lock()
+	defer s.qMu.Unlock()
+
+	// TODO: Optimize this first if it becomes an issue.
+	for queueName, q := range s.queues {
+		idx := slices.IndexFunc(q, func(j job) bool {
+			return j.ID == jobID
+		})
+
+		if idx > -1 {
+			// Job found
+			job := q[idx]
+			// Move the job to the deleted map
+			s.deleted[job.ID] = job
+			// remove from queue
+			q = slices.Delete(q, idx, 1)
+			s.queues[queueName] = q
+			return job, queueName, nil
+		}
+	}
+
+	// Check if assigned
+	for _, j := range s.assigned {
+		if j.ID == jobID {
+			return j, "assigned", nil
+		}
+	}
+	return job{}, "", ErrNoJob
 }
--- 9-job-centre/inmem/inmem_test.go
+++ 9-job-centre/inmem/inmem_test.go
@@ -15,7 +15,7 @@ type testJob struct {
 	payload   json.RawMessage
 }
 
-func TestInsert(t *testing.T) {
+func TestAddJob(t *testing.T) {
 	toInsert := []testJob{
 		{
 			queueName: "test",
@@ -39,18 +39,22 @@ func TestInsert(t *testing.T) {
 	clientID := uint64(1)
 
 	for _, ti := range toInsert {
-		err := q.AddJob(ctx, clientID, ti.queueName, ti.pri, ti.payload)
+		job, err := q.AddJob(ctx, clientID, ti.queueName, ti.pri, ti.payload)
 		require.NoError(t, err)
+
+		require.NotEmpty(t, job.ID)
+		require.Equal(t, ti.payload, job.Payload)
+		require.Equal(t, ti.pri, job.Pri)
 	}
 }
 
-func TestGet(t *testing.T) {
+func TestNextJob(t *testing.T) {
 	t.Run("one job", func(t *testing.T) {
 		clientID := uint64(1)
 		ctx := context.Background()
 		q := inmem.NewQueue()
 
-		err := q.AddJob(ctx, clientID, "q1", 1, json.RawMessage(`{"test": "test"}`))
+		_, err := q.AddJob(ctx, clientID, "q1", 1, json.RawMessage(`{"test": "test"}`))
 		require.NoError(t, err)
 
 		j, queueName, err := q.NextJob(ctx, clientID, []string{"q1"})
@@ -64,11 +68,13 @@ func TestGet(t *testing.T) {
 		ctx := context.Background()
 		q := inmem.NewQueue()
 
-		err := q.AddJob(ctx, clientID, "queue1", 1, json.RawMessage(`{"test": "test"}`))
+		job, err := q.AddJob(ctx, clientID, "queue1", 1, json.RawMessage(`{"test": "test"}`))
 		require.NoError(t, err)
+		require.NotEmpty(t, job.ID)
 
-		err = q.AddJob(ctx, clientID, "queue1", 2, json.RawMessage(`{"test": "test"}`))
+		job, err = q.AddJob(ctx, clientID, "queue1", 2, json.RawMessage(`{"test": "test"}`))
 		require.NoError(t, err)
+		require.NotEmpty(t, job.ID)
 
 		j, queueName, err := q.NextJob(ctx, clientID, []string{"queue1"})
 		require.NoError(t, err)
@@ -79,7 +85,6 @@ func TestGet(t *testing.T) {
 	t.Run("retrieve highest priority from all queues", func(t *testing.T) {
 		clientID := uint64(1)
 		ctx := context.Background()
-		ctx = context.WithValue(ctx, "clientID", 1)
 		q := inmem.NewQueue()
 
 		jobs := []testJob{
@@ -96,7 +101,7 @@ func TestGet(t *testing.T) {
 		}
 
 		for _, job := range jobs {
-			err := q.AddJob(ctx, clientID, job.queueName, job.pri, job.payload)
+			_, err := q.AddJob(ctx, clientID, job.queueName, job.pri, job.payload)
 			require.NoError(t, err)
 		}
 
@@ -108,7 +113,7 @@ func TestGet(t *testing.T) {
 		j, queueName, err = q.NextJob(ctx, clientID, []string{"queue2", "queue1"})
 		require.NoError(t, err)
 		require.Equal(t, "queue1", queueName)
-		require.Equal(t, int64(2), j.Pri)
+		require.Equal(t, int64(1), j.Pri)
 	})
 
 	t.Run("job unavailable after assigned", func(t *testing.T) {
@@ -125,7 +130,7 @@ func TestGet(t *testing.T) {
 		q := inmem.NewQueue()
 
 		for _, job := range jobs {
-			err := q.AddJob(ctx, clientID1, job.queueName, job.pri, job.payload)
+			_, err := q.AddJob(ctx, clientID1, job.queueName, job.pri, job.payload)
 			require.NoError(t, err)
 		}
 
@@ -135,8 +140,75 @@ func TestGet(t *testing.T) {
 		require.Equal(t, int64(1), j.Pri)
 
 		j, queueName, err = q.NextJob(ctx, clientID2, []string{"test"})
-		require.Equal(t, inmem.ErrNoJob, err)
+		require.ErrorIs(t, err, inmem.ErrNoJob)
 		require.Equal(t, "", queueName)
 		require.Empty(t, j)
 	})
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("Delete assigned job", func(t *testing.T) {
+		jobs := []testJob{
+			{
+				queueName: "test",
+				pri:       40,
+			},
+
+			{
+				queueName: "test",
+				pri:       300,
+			},
+		}
+
+		clientID := uint64(1)
+		ctx := context.Background()
+		q := inmem.NewQueue()
+
+		for _, job := range jobs {
+			_, err := q.AddJob(ctx, clientID, job.queueName, job.pri, job.payload)
+			require.NoError(t, err)
+		}
+
+		j, queueName, err := q.NextJob(ctx, clientID, []string{"test"})
+		require.NoError(t, err)
+		require.Equal(t, "test", queueName)
+		require.Equal(t, int64(300), j.Pri)
+		require.NotEmpty(t, j.ID)
+
+		err = q.DeleteJob(ctx, clientID, j.ID)
+		require.NoError(t, err)
+
+		j, queueName, err = q.NextJob(ctx, clientID, []string{"test"})
+		require.NoError(t, err)
+		require.Equal(t, "test", queueName)
+		require.Equal(t, int64(40), j.Pri)
+
+		j, queueName, err = q.NextJob(ctx, clientID, []string{"test"})
+		require.ErrorIs(t, err, inmem.ErrNoJob)
+		require.Empty(t, j)
+		require.Empty(t, queueName)
+	})
+
+	t.Run("delete available job", func(t *testing.T) {
+		ctx := context.Background()
+		clientID := uint64(123)
+		job := testJob{
+			queueName: "test",
+			pri:       300,
+		}
+		q := inmem.NewQueue()
+		queued, err := q.AddJob(ctx, clientID, job.queueName, job.pri, job.payload)
+		require.NoError(t, err)
+		require.NotEmpty(t, queued.ID)
+
+		err = q.DeleteJob(ctx, clientID, queued.ID)
+		require.NoError(t, err)
+
+		_, _, err = q.NextJob(ctx, clientID, []string{"test"})
+		require.ErrorIs(t, err, inmem.ErrNoJob)
+
+		err = q.DeleteJob(ctx, clientID, queued.ID)
+		require.ErrorIs(t, err, inmem.ErrNoJob, "job should already be deleted")
+	})
+
 }
